6f12890142dea6bd3c0780810154d1f699a46797
birinci novbe <birinci novbe@DESKTOP-R4EGFJU>
2023-10-27 11:11:02 +0400

    Add project files.

---
+++ abstrakt/Figure.cs
@@ -0,0 +1,102 @@
+﻿using abstrakt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace abstrakt
+{
+    internal abstract class Figure
+    {
+
+        public abstract int CalcArea();
+       
+    }
+    internal class Square: Figure
+    {
+        private int side;
+
+        public Square(int side)
+        {
+            Side = side;
+        }
+
+        public int Side { get { return side; } 
+            set 
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Enter a non negative int");
+                }
+                else
+                {
+                    side = value;
+                }
+
+            } 
+        }
+
+        
+
+        public override int CalcArea() 
+        { 
+            if (Side <= 0) 
+            {
+                return -1;
+            }
+            return Side * Side;
+        }
+    }
+    internal class Rectangle : Figure
+    {
+        private int lenght;
+        private int width;
+
+        public Rectangle(int lenght, int width)
+        {
+            Lenght = lenght;
+            Width = width;
+        }
+
+        public int Lenght {  get { return lenght;} 
+            set 
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Enter a non negative integer");
+                }
+                else
+                {
+                    lenght = value;
+                }
+            } 
+        } 
+        public int Width { get { return width; } 
+            set 
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Enter a non negative integer");
+                }
+                else
+                {
+                   width = value;
+                }
+            } 
+        }
+
+        public override int CalcArea()
+        {
+            if (Width <= 0 || Lenght <=0)
+            {
+                return -2;
+            }
+            return Lenght * Width;
+        }
+    }
+}
+
+
---
+++ abstrakt/Program.cs
@@ -0,0 +1,31 @@
+﻿using abstrakt;
+
+Console.WriteLine("1.Square" + "\n2.Rectangular" + "\nQuit");
+
+Figure figure;   
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
+{
+    case 0:
+        break;
+        case 1:
+        figure = new Square(0);
+        int a = figure.CalcArea();
+        
+        if( a == -1)
+        {
+            break;
+        }
+        Console.WriteLine(a);
+
+        break;
+            case 2:
+        figure = new Rectangle(-5,7);
+        int b = figure.CalcArea();
+        if( b == -2)
+        {
+            break;
+        }
+        Console.WriteLine(b);
+        break;
+}
